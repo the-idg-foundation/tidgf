9a56641c68ebb1097277ee1b3142e75bf4898477
Kirilenko Danila <33377623+gc-group@users.noreply.github.com>
2017-12-15 19:58:53 +0300

    added email

--- scripts/contact.php
+++ scripts/contact.php
@@ -1,9 +1,9 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if(!empty($_POST['contactname']) && !empty($_POST['contactemail']) && !empty($_POST['contactmessage'])) {
-	$to = 'your@email.com'; // Your e-mail address here.
+	$to = 'kirilenko.danila@gmail.com'; // Your e-mail address here.
 	$body = "\nName: {$_POST['contactname']}\nEmail: {$_POST['contactemail']}\n\n\n{$_POST['contactmessage']}\n\n";
-	mail($to, "Message from yoursite.com", $body, "From: {$_POST['contactemail']}"); // E-Mail subject here.
+	mail($to, "Message from https://the-idg-foundation.github.io/site.tidgf/", $body, "From: {$_POST['contactemail']}"); // E-Mail subject here.
     }
 }
 ?>
